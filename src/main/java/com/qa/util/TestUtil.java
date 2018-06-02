1976c202ae3fb0f65eebdf75a1ac95b8c318e24a
Rohith Singirikonda <singirikonda.rohith@gmail.com>
2018-06-03 00:38:43 +0530

    1. Basic Structure of Project Commited

---
+++ src/main/java/com/qa/util/TestUtil.java
@@ -0,0 +1,14 @@
+package com.qa.util;
+
+import com.qa.testbase.TestBase;
+
+public class TestUtil extends TestBase {
+	
+		public static int PageLoadTimeOut = 20;
+		public static int Implicit_Wait = 20;
+		
+		
+		
+	
+
+}
